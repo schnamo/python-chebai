ead30072eed9c2c1a17bc8f7ac5dd79c0b90c476
schnamo <ch.tumescheit@gmail.com>
2024-06-05 18:01:17 +0200

    adjusting new class

--- chebai/preprocessing/datasets/solCuration.py
+++ chebai/preprocessing/datasets/solCuration.py
@@ -21,18 +21,7 @@ from chebai.preprocessing.datasets.pubchem import Hazardous
 
 class SolCuration(XYBaseDataModule):
     HEADERS = [
-        "NR-AR",
-        "NR-AR-LBD",
-        "NR-AhR",
-        "NR-Aromatase",
-        "NR-ER",
-        "NR-ER-LBD",
-        "NR-PPAR-gamma",
-        "SR-ARE",
-        "SR-ATAD5",
-        "SR-HSE",
-        "SR-MMP",
-        "SR-p53",
+        "logS",
     ]
 
     @property
@@ -53,17 +42,18 @@ class SolCuration(XYBaseDataModule):
 
     def download(self):
         with NamedTemporaryFile("rb") as gout:
+            # start with downloading just one part of the dataset, later add the remaining ones
             request.urlretrieve(
-                "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/tox21.csv.gz",
+                "https://github.com/Mengjintao/SolCuration/blob/master/cure/esol_cure.csv",
                 gout.name,
             )
-            with gzip.open(gout.name) as gfile:
-                with open(os.path.join(self.raw_dir, "tox21.csv"), "wt") as fout:
-                    fout.write(gfile.read().decode())
+            with gout.name as gfile:
+               with open(os.path.join(self.raw_dir, "solCuration.csv"), "wt") as fout:
+                   fout.write(gfile.read())
 
     def setup_processed(self):
         print("Create splits")
-        data = self._load_data_from_file(os.path.join(self.raw_dir, f"tox21.csv"))
+        data = self._load_data_from_file(os.path.join(self.raw_dir, f"solCuration.csv"))
         groups = np.array([d["group"] for d in data])
         if not all(g is None for g in groups):
             split_size = int(len(set(groups)) * self.train_split)
@@ -124,167 +114,15 @@ class SolCuration(XYBaseDataModule):
             self.setup_processed()
 
     def _load_dict(self, input_file_path):
+        i = 0
         with open(input_file_path, "r") as input_file:
             reader = csv.DictReader(input_file)
             for row in reader:
                 smiles = row["smiles"]
-                labels = [
-                    bool(int(l)) if l else None for l in (row[k] for k in self.HEADERS)
-                ]
-                yield dict(features=smiles, labels=labels, ident=row["mol_id"])
+                labels = row["logS"]
+                # dataset has no mol_id TODO
+                yield dict(features=smiles, labels=labels, ident=i) #, ident=row["mol_id"]
+                i += 1
 
-
-class Tox21Challenge(XYBaseDataModule):
-    HEADERS = [
-        "NR-AR",
-        "NR-AR-LBD",
-        "NR-AhR",
-        "NR-Aromatase",
-        "NR-ER",
-        "NR-ER-LBD",
-        "NR-PPAR-gamma",
-        "SR-ARE",
-        "SR-ATAD5",
-        "SR-HSE",
-        "SR-MMP",
-        "SR-p53",
-    ]
-
-    @property
-    def _name(self):
-        return "Tox21Chal"
-
-    @property
-    def label_number(self):
-        return 12
-
-    @property
-    def raw_file_names(self):
-        return [
-            "train.sdf",
-            "validation.sdf",
-            "validation.smiles",
-            "test.smiles",
-            "test_results.txt",
-        ]
-
-    @property
-    def processed_file_names(self):
-        return ["test.pt", "train.pt", "validation.pt"]
-
-    def download(self):
-        self._retrieve_file(
-            "https://tripod.nih.gov/tox21/challenge/download?id=tox21_10k_data_allsdf&sec=",
-            "train.sdf",
-            compression="zip",
-        )
-        self._retrieve_file(
-            "https://tripod.nih.gov/tox21/challenge/download?id=tox21_10k_challenge_testsdf&sec=",
-            "validation.sdf",
-            compression="zip",
-        )
-        self._retrieve_file(
-            "https://tripod.nih.gov/tox21/challenge/download?id=tox21_10k_challenge_scoresmiles&sec=",
-            "test.smiles",
-        )
-        self._retrieve_file(
-            "https://tripod.nih.gov/tox21/challenge/download?id=tox21_10k_challenge_scoretxt&sec=",
-            "test_results.txt",
-        )
-
-    def _retrieve_file(self, url, target_file, compression=None):
-        target_path = os.path.join(self.raw_dir, target_file)
-        if not os.path.isfile(target_path):
-            with NamedTemporaryFile("rb") as gout:
-                if compression is None:
-                    download_path = target_path
-                else:
-                    download_path = gout.name
-                request.urlretrieve(
-                    url,
-                    download_path,
-                )
-                if compression == "zip":
-                    td = TemporaryDirectory()
-                    with zipfile.ZipFile(download_path, "r") as zip_ref:
-                        zip_ref.extractall(td.name)
-                        files_in_zip = os.listdir(td.name)
-                        f = files_in_zip[0]
-                        assert len(files_in_zip) == 1
-                        shutil.move(os.path.join(td.name, f), target_path)
-
-    def _load_data_from_file(self, path):
-        sdf = Chem.SDMolSupplier(path)
-        data = []
-        for mol in sdf:
-            if mol is not None:
-                d = dict(
-                    labels=[
-                        int(mol.GetProp(h)) if h in mol.GetPropNames() else None
-                        for h in self.HEADERS
-                    ],
-                    ident=[
-                        mol.GetProp(k)
-                        for k in ("DSSTox_CID", "Compound ID")
-                        if k in mol.GetPropNames()
-                    ][0],
-                    features=Chem.MolToSmiles(mol),
-                )
-                data.append(self.reader.to_data(d))
-        return data
-
-    def setup_processed(self):
-        for k in ("train", "validation"):
-            d = self._load_data_from_file(os.path.join(self.raw_dir, f"{k}.sdf"))
-            torch.save(d, os.path.join(self.processed_dir, f"{k}.pt"))
-
-        with open(os.path.join(self.raw_dir, f"test.smiles")) as fin:
-            next(fin)
-            test_smiles = dict(reversed(row.strip().split("\t")) for row in fin)
-        with open(os.path.join(self.raw_dir, f"test_results.txt")) as fin:
-            headers = next(fin).strip().split("\t")
-            test_results = {
-                k["Sample ID"]: [
-                    int(k[h]) if k[h] != "x" else None for h in self.HEADERS
-                ]
-                for k in (
-                    dict(zip(headers, row.strip().split("\t"))) for row in fin if row
-                )
-            }
-        test_data = [
-            self.reader.to_data(
-                dict(features=test_smiles[k], labels=test_results[k], ident=k)
-            )
-            for k in test_smiles
-        ]
-        torch.save(test_data, os.path.join(self.processed_dir, f"test.pt"))
-
-    def setup(self, **kwargs):
-        if any(
-            not os.path.isfile(os.path.join(self.raw_dir, f))
-            for f in self.raw_file_names
-        ):
-            self.download()
-        if any(
-            not os.path.isfile(os.path.join(self.processed_dir, f))
-            for f in self.processed_file_names
-        ):
-            self.setup_processed()
-
-    def _load_dict(self, input_file_path):
-        with open(input_file_path, "r") as input_file:
-            reader = csv.DictReader(input_file)
-            for row in reader:
-                smiles = row["smiles"]
-                labels = [
-                    bool(int(l)) if l else None for l in (row[k] for k in self.HEADERS)
-                ]
-                yield dict(features=smiles, labels=labels, ident=row["mol_id"])
-
-
-class Tox21ChallengeChem(Tox21Challenge):
-    READER = dr.ChemDataReader
-
-
-class Tox21MolNetChem(Tox21MolNet):
+class SolubilityCuratedData(SolCuration):
     READER = dr.ChemDataReader
