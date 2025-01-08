873cacb8bf31c6c9ce06e86733c1c0ed72c5dd7b
schnamo <ch.tumescheit@gmail.com>
2025-01-08 16:46:59 +0100

    add solubility class for diff dataset (not finished yet)

--- chebai/preprocessing/datasets/solCuration.py
+++ chebai/preprocessing/datasets/solCuration.py
@@ -157,3 +157,104 @@ class SolCurationChem(SolCuration):
     """Chemical data reader for the solubility dataset."""
 
     READER = dr.ChemDataReader
+
+
+
+class SolCurationKaggle(XYBaseDataModule):
+    HEADERS = [
+        "logS",
+    ]
+
+    @property
+    def _name(self):
+        return "SolCuration"
+
+    @property
+    def label_number(self):
+        return 3
+
+    @property
+    def raw_file_names(self):
+        return ["train.csv"]
+
+    @property
+    def processed_file_names(self):
+        return ["test.pt", "train.pt", "validation.pt"]
+
+    # def download(self):
+        # # download and combine all the available curated datasets from xxx
+        # db_sol = ['aqsol','aqua','esol','ochem','phys']
+        # with open(os.path.join(self.raw_dir, "solCuration.csv"), "ab") as dst:
+        #     for i, db in enumerate(db_sol):
+        #         with request.urlopen(f"https://raw.githubusercontent.com/Mengjintao/SolCuration/master/cure/{db}_cure.csv",) as src:
+        #             if i > 0:
+        #                 src.readline()
+        #             shutil.copyfileobj(src, dst)
+             
+
+    def setup_processed(self):
+        print("Create splits")
+        data = self._load_data_from_file(os.path.join(self.raw_dir, f"solCuration.csv"))
+        if 0 == 0:
+            train_split, test_split = train_test_split(
+                data, train_size=self.train_split, shuffle=True
+            )
+            test_split, validation_split = train_test_split(
+                test_split, train_size=0.5, shuffle=True
+            )
+        for k, split in [
+            ("test", test_split),
+            ("train", train_split),
+            ("validation", validation_split),
+        ]:
+            print("transform", k)
+            torch.save(
+                split,
+                os.path.join(self.processed_dir, f"{k}.pt"),
+            )
+
+    def setup(self, **kwargs):
+        if any(
+            not os.path.isfile(os.path.join(self.raw_dir, f))
+            for f in self.raw_file_names
+        ):
+            self.download()
+        print([
+            not os.path.isfile(os.path.join(self.processed_dir, f))
+            for f in self.processed_file_names
+        ])
+        if any(
+            not os.path.isfile(os.path.join(self.processed_dir, f))
+            for f in self.processed_file_names
+        ):
+            self.setup_processed()
+
+    def _load_data_from_file(self, input_file_path: str) -> List[Dict]:
+        """Loads data from a CSV file.
+
+        Args:
+            input_file_path (str): Path to the CSV file.
+
+        Returns:
+            List[Dict]: List of data dictionaries.
+        """
+        smiles_l = []
+        labels_l = []
+        print('READING IN DATA: ', input_file_path)
+        # print('MODEL: ', self.model_type)
+        with open(input_file_path, "r") as input_file:
+            reader = csv.DictReader(input_file)
+            for row in reader:
+                smiles_l.append(row["smiles"])
+                labels_l.append(int(row["sol_category"]))
+
+        for i in range(0,len(smiles_l)):
+            yield dict(features=smiles_l[i], labels=[labels_l[i]], ident=i)
+
+class SolubilityCuratedData(SolCuration):
+    READER = dr.ChemDataReader
+
+class SolCurationChem(SolCuration):
+    """Chemical data reader for the solubility dataset."""
+
+    READER = dr.ChemDataReader
